77e77adadc3bf961d851efe5c5096a03f3cfeee2
Rickson Rocha <ricksonroccha21@gmail.com>
2025-01-17 22:36:09 -0300

    [refact]: add new fields in tb_project

--- src/main/resources/db/migration/V1__create_table_project.sql
+++ src/main/resources/db/migration/V1__create_table_project.sql
@@ -4,7 +4,10 @@ CREATE TYPE project_status AS ENUM ('NEW', 'IN_PROGRESS', 'DONE', 'DELAYED', 'CA
 
 CREATE TABLE tb_project (
     id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
-    description VARCHAR(30) NOT NULL CHECK (LENGTH(description) >= 4 AND LENGTH(description) <= 30),
-    project_status project_status NOT NULL
+    name VARCHAR(255) NOT NULL CHECK (LENGTH(name) >= 4 AND LENGTH(name) <= 255),
+    description VARCHAR(255) NOT NULL CHECK (LENGTH(description) >= 4 AND LENGTH(description) <= 255),
+    project_status project_status NOT NULL,
+    start_date DATE NOT NULL CHECK (start_date >= CURRENT_DATE),
+    end_date DATE NOT NULL CHECK (end_date > CURRENT_DATE),
+    CHECK (end_date > start_date)
 );
