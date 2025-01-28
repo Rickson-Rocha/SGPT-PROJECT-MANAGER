ebe1b5891cd14afbccd299a2f1fa32f7cbacd012
Rickson Rocha <ricksonroccha21@gmail.com>
2025-01-28 13:20:23 -0300

    [feat]:create TaskNotFoundException

---
+++ src/main/java/com/br/sgpt/projectmanager/exceptions/TaskNotFoundException.java
@@ -0,0 +1,10 @@
+package com.br.sgpt.projectmanager.exceptions;
+
+public class TaskNotFoundException  extends RuntimeException {
+    public TaskNotFoundException(String message) {
+        super(message);
+    }
+    public TaskNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
